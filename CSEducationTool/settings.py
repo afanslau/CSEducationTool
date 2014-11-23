f04382b37b49787a205b7f1b99718288571ffc3c
Adam Fanslau <afanslau@gmail.com>
2014-11-23 16:03:47 -0500

    set up static files

--- CSEducationTool/settings.py
+++ CSEducationTool/settings.py
@@ -87,7 +87,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 #Don't actually need this, since the django.template.loaders.app_directories.Loader looks for the templates directory under each running app.
 # TEMPLATE_DIRS = ('/templates/',)
