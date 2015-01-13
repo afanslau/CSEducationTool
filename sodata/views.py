a733d55bf14f00b12a0fb2e455b3ee2126d52c6f
Adam Fanslau <afanslau@gmail.com>
2015-01-13 14:24:28 -0500

    Merge views.py

--- sodata/views.py
+++ sodata/views.py
@@ -22,37 +22,9 @@ ui_topic_base_url = 'resources/%d'
 
 ''' Render and return the topic template page '''
 def ui_get_resource(request, resource_id=None):
-<<<<<<< HEAD
-
-	if resource_id is None:
-		#Return top level resources
-
-		# Parent is fake, just for display purposes. This is a bad way to do this, violates MVC
-		parent = Resources(title='Home',text='This is a website where you can explore computer science topics. You can store good resources you find, and share them with others.')
-		resources = Resources.objects.filter(parent_resources=None)
-		resources = sorted(resources, key=lambda t: t.updated_at, reverse=True)
-		plist = []
-
-	else:
-		#Return one specific topic
-		resource_id = int(resource_id)
-		try:
-			parent = Resources.objects.get(id=resource_id)
-			resources = parent.get_child_resources()
-			plist = parent.get_parent_resources()
-			#Sort by updated_at
-			if resources is not None:
-				resources = sorted(resources, key=lambda t: t.updated_at, reverse=True) # , reverse=True)
-			print('request - Render template with resource_id: %d  %s' % (parent.id, parent.title))
-		except ObjectDoesNotExist:
-			print('request - Render template with resource_id: %d  NOT FOUND' % resource_id)
-			raise Http404
-
-	data = {'topic':parent, 'resource_list':resources, 'parent_list':plist}
-	return render(request, 'sodata/topic.html', data) #same-as  render(request, 'sodata/index.html', data)
-=======
     data = get_resource(request, resource_id)
     return render(request,'sodata/newtopic.html', data) #same-as  render(request, 'sodata/index.html', data)
+
 def get_resource(request, resource_id=None):
 
     #Default for anon users
@@ -154,8 +126,6 @@ def ui_edit_resource(request):
         form = ResourceForm()
 
     return render(request, 'resource_form.html', {'form': form})
->>>>>>> devmerge
-
 
 ''' GET /resources/{id} - get a single resource '''
 def api_get_resource(request, resource_id):
@@ -170,30 +140,7 @@ def api_get_resource(request, resource_id):
 
 
 def ui_search_resources(request):
-<<<<<<< HEAD
-	''' Basic keyword search attempt '''
-	query_string = ''
-	found_entries = None
 
-	if ('q' in request.GET) and request.GET['q'].strip():
-		query_string = request.GET['q']
-		entry_query = get_query(query_string, ['title', 'text'])		
-		found_entries = Resources.objects.filter(entry_query).order_by('-rating')
-	else: found_entries = Resources.objects.all()
-
-	if ('seed' in request.GET):
-		val = request.GET['seed'].strip()
-		if val=="True" or val=="1":
-			seed = True
-		elif val=="False" or val=="0":
-			seed = False
-		else: 
-			return HttpResponseBadRequest
-		found_entries = found_entries.filter(pre_seeded=seed)
-	# Fake a resource for easy diplay
-	parent = Resources(title="Search Results", text=query_string)
-	return render(request, 'sodata/topic.html', { 'topic': parent, 'resource_list': found_entries })
-=======
     ''' Basic keyword search attempt '''
     query_string = ''
     found_entries = None
@@ -216,7 +163,6 @@ def ui_search_resources(request):
     # Fake a resource for easy diplay
     parent = Resources(title="Search Results", text=query_string)
     return HttpResponse("Search not Implemented")
->>>>>>> devmerge
 
 
 def api_search_resources(request):
@@ -236,73 +182,9 @@ def api_create_resource(request, parent_id=None):
         return HttpResponseBadRequest("Invalid Form Data")
 
 def ui_create_resource(request, parent_id=None):
-<<<<<<< HEAD
-	new_resource = create_resource(request, parent_id)
-	data = {'resource':new_resource}
-	return render(request,'sodata/list_item.html', data)
-
-def create_resource(request, parent_id=None):
-
-	parsed_urls = []
-	if request.POST.get('text') is not None and request.POST.get('url') is None:
-		parsed_urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', request.POST.get('text'))
-		print(parsed_urls)
-
-	new_url = request.POST.get('url')
-	if len(parsed_urls)>0 and (new_url is None or new_url == ''):
-		new_url = parsed_urls[0]
-
-	
-	new_resource = Resources(title=request.POST.get('title'), text=request.POST.get('text'), url=new_url)
-
-	# new_resource.save()
-
-	if parent_id is None:
-		print(new_resource.id)
-		new_resource.save()
-		print(new_resource.id)
-		print('request - CREATE resource with data %s' % request.POST)
-	else:
-		try:
-			parent_id = int(parent_id)
-			parent = Resources.objects.get(id=parent_id)
-		except ObjectDoesNotExist:
-			print('request - CREATE resource under %d  NOT FOUND' % parent_id)
-			raise Http404
-		print(new_resource.id)
-		new_resource.save()
-		print(new_resource.id)
-		TopicRelations(from_resource=parent, to_resource=new_resource).save()
-		print('request - CREATE resource under %s with data %s' % (parent.title, new_resource.to_dict()))
-
-	print(new_resource.to_dict())
-	return new_resource
-
-
-def api_update_resource(request, resource_id=None):
-	if resource_id is None: raise Http404
-	updated_resource = update_resource(request, resource_id)
-	return json_response(updated_resource)
-
-def ui_update_resource(request, resource_id=None):
-	if resource_id is None: raise Http404
-	updated_resource = update_resource(request, resource_id)
-	data = {'resource':updated_resource}
-	return render(request, 'sodata/resource_content.html', data)
-
-def update_resource(request, resource_id=None):
-	print('request - UPDATE resource with data %s' % request.POST)
-	
-	# Get the resource
-	try:
-		resource = Resources.objects.get(id=resource_id)
-	except ObjectDoesNotExist:
-		raise Http404
-=======
     print 'ui_create_resource',request.POST
     pid = int(parent_id) if parent_id is not None else None
     new_resource = create_resource(request, pid)
->>>>>>> devmerge
 
     if new_resource is not None:
         rid = request.POST.get("current_resource_id")
@@ -488,21 +370,13 @@ def api_create_relation(request, parent_id, child_id):
     return HttpResponse(json.dumps(relation.to_dict()))
 
 def create_relation(parent_id, child_id):
-<<<<<<< HEAD
-	parent = Resources.objects.get(id=parent_id)
-	child = Resources.objects.get(id=child_id)
-	relation, created = TopicRelations.objects.get_or_create(from_resource=parent, to_resource=child)
-	relation.save()
-	print('request - Create relation from %s to %s' % (parent.title, child.title))
-	return relation 
-=======
     parent = Resources.objects.get(id=parent_id)
     child = Resources.objects.get(id=child_id)
     relation = TopicRelations(from_resource=parent, to_resource=child)
     relation.save()
     print('request - Create relation from %s to %s' % (parent.title, child.title))
     return relation 
->>>>>>> devmerge
+
 def api_delete_relation_by_id(request, relation_id):
     relation_id = int(relation_id)
     try:
