b5aed8a125e2e8208c21894087db44f818c9f34a
Adam Fanslau <afanslau@gmail.com>
2015-03-01 23:04:45 -0500

    list_users command

---
+++ sodata/management/commands/list_users.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import Client
+from django.test import TestCase, RequestFactory
+
+class Command(BaseCommand):
+	help = 'lists all usernames'
+	def handle(self, *args, **options):
+		i = 0
+		for u in User.objects.all():
+			print u.username
+			i += 1
+		print 'Total number of users is ', i
