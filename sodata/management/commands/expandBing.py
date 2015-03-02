7be126bc74f95818caa248d3feeafa5104371147
Adam Fanslau <afanslau@gmail.com>
2015-03-02 00:27:05 -0500

    Made urls unique in expandBing

--- sodata/management/commands/expandBing.py
+++ sodata/management/commands/expandBing.py
@@ -27,11 +27,14 @@ class Command(BaseCommand):
 				counted_errors = 0
 				for entry in entries:
 					# try:
-					title = entry.content.find('d:title').get_text()
-					url = entry.content.find('d:url').get_text()
-					displayurl = entry.content.find('d:displayurl').get_text()
-					text = entry.content.find('d:description').get_text()
-					new, created = Resources.objects.get_or_create(title=title, text=text, url=url, display_url=displayurl, author=bing_user)
+					new.url = entry.content.find('d:url').get_text()
+					new, created = Resources.objects.get_or_create(url=url)
+					if created:
+						title = entry.content.find('d:title').get_text()	
+						new.displayurl = entry.content.find('d:displayurl').get_text()
+						new.text = entry.content.find('d:description').get_text()
+						new.author = bing_user
+
 					TopicRelations.objects.get_or_create(to_resource=new, from_resource=tagr, perspective_user=bing_user)
 					# except Exception:
 					# 	counted_errors += 1
